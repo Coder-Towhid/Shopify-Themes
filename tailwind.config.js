17895ce7787054ff145d4bf14c242dbde58db415
Coder-Towhid <islamsmtowhid@gmail.com>
2024-05-28 10:25:40 +0600

    Intilizing with two different collection theme

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,liquid}"],
+  theme: {
+    extend: {
+      height: {
+        '94': '22rem'
+      }
+    
+    },
+  },
+  plugins: [],
+}
+
