d3305e077524d3ddf5ea275653483dd62fa4ad92
Coder-Towhid <islamsmtowhid@gmail.com>
2024-06-03 14:42:40 +0600

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,18 +2,10 @@
 module.exports = {
   content: ["./**/*.{html,liquid}"],
   theme: {
-    extend: {
-      height: {
-        '94': '22rem'
-      },
-      gridTemplateColumns: {
-        // Simple 3 column grid
-        '3': 'repeat(3, minmax(0, 1fr))'
-      }
-
-    
-    },
+    extend: {},
   },
+  safelist: [
+    'grid-cols-3'
+  ],
   plugins: [],
 }
-
