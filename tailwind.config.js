fa555ad959645069be3effd4b69756e3d9e2ce40
Coder-Towhid <islamsmtowhid@gmail.com>
2024-06-03 11:33:22 +0600

    working on custom tailwind class

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ module.exports = {
     extend: {
       height: {
         '94': '22rem'
+      },
+      gridTemplateColumns: {
+        // Simple 3 column grid
+        '3': 'repeat(3, minmax(0, 1fr))'
       }
+
     
     },
   },
